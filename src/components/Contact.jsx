ea1f7d05e2c8aae8ba704688fd75935a24ddcc04
khushboo-590 <khushbook4979@gmail.com>
2025-05-16 15:23:18 +0530

    add link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const Contact = () => {
     return (
         <div className='max-w-[1320px] mx-auto px-6 py-3'>
-            <h2 className='text-green-700 text-2xl mt-8 text-center'>Nested nav link example</h2>
+            <h2 className='text-green-700 text-2xl mt-8 text-center'>Nested link example (without NavLink)</h2>
             <nav className='flex gap-10 justify-center mt-4 mb-4 text-lg'>
-                <NavLink to="contactform" className={({ isActive }) => isActive
-                    ? 'text-red-500 font-bold'
-                    : 'text-purple-700 hover:text-red-500 font-bold'
-                }
+                <Link
+                    to="contactform"
+                    className='text-purple-700 hover:text-red-500 font-bold'
                 >
                     Form
-                </NavLink>
-                <NavLink to="link2" className={({ isActive }) => isActive
-                    ? 'text-red-500 font-bold'
-                    : 'text-purple-700 hover:text-red-500 font-bold'
-                }
+                </Link>
+                <Link
+                    to="link2"
+                    className='text-purple-700 hover:text-red-500 font-bold'
                 >
                     Link-2
-                </NavLink>
+                </Link>
             </nav>
             <Outlet />
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { NavLink,Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { navLinks } from '../utils/helper';
 
 const Navbar = () => {
@@ -9,16 +9,18 @@ const Navbar = () => {
         setMenuOpen(!menuOpen);
         document.body.classList.toggle("overflow-hidden", !menuOpen);
     };
-   return (
+
+    return (
         <nav className="px-6 py-5 flex items-center justify-between max-w-[1320px] mx-auto relative">
             <Link to='/' className="text-white text-3xl font-semibold items-center">Page logo</Link>
             <div className="hidden md:flex space-x-6">
-                {navLinks.map((link,index) => (
-                    <NavLink key={index}  to={link.path}
-                        className={({ isActive }) =>
-                            `font-bold ${isActive ? 'text-red-500' : 'text-white'}`}    >
+                {navLinks.map((link, index) => (
+                    <Link
+                        key={index}
+                        to={link.path}
+                        className="font-bold text-white hover:text-red-500">
                         {link.name}
-                    </NavLink>
+                    </Link>
                 ))}
             </div>
             <button className="hidden md:block bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md">
@@ -26,18 +28,23 @@ const Navbar = () => {
             </button>
             <div onClick={toggleNavbar} className="md:hidden cursor-pointer z-30 flex flex-col gap-1">
                 <span className={`bg-white block w-6 h-1 transition-all duration-300 ${menuOpen && "rotate-45 translate-y-[8px]"}`}></span>
-               <span className={`block w-6 h-1 bg-white transition-all duration-300 ${menuOpen && "opacity-0"}`}></span>
-               <span className={`block w-6 h-1 bg-white transition-all duration-300 ${menuOpen && "-rotate-45 -translate-y-[8px]"}`}></span>
+                <span className={`block w-6 h-1 bg-white transition-all duration-300 ${menuOpen && "opacity-0"}`}></span>
+                <span className={`block w-6 h-1 bg-white transition-all duration-300 ${menuOpen && "-rotate-45 -translate-y-[8px]"}`}></span>
             </div>
             <div
-                className={`fixed top-0 right-0 w-full h-screen bg-black z-20 flex flex-col items-center justify-center space-y-6 transform transition-transform duration-500  ${menuOpen ? 'translate-x-0' : 'translate-x-full'
-                    } md:hidden`}>
-                {navLinks.map((link,index) => (
-                    <NavLink key={index}  to={link.path} className={({ isActive }) =>
-                            `font-bold text-lg ${isActive ? 'text-red-500' : 'text-white'}`}
-                        onClick={() => setMenuOpen(false)}>{link.name}</NavLink>
+                className={`fixed top-0 right-0 w-full h-screen bg-black z-20 flex flex-col items-center justify-center space-y-6 transform transition-transform duration-500 ${menuOpen ? 'translate-x-0' : 'translate-x-full'} md:hidden`}          >
+                {navLinks.map((link, index) => (
+                    <Link
+                        key={index}
+                        to={link.path}
+                        className="font-bold text-lg text-white hover:text-red-500"
+                        onClick={() => setMenuOpen(false)} >
+                        {link.name}
+                    </Link>
                 ))}
-                <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md">Get Started</button>
+                <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md">
+                    Get Started
+                </button>
             </div>
         </nav>
     );
