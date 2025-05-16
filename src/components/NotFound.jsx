82329a761d50afa50b55caa1caa37a2b33abec59
khushboo-590 <khushbook4979@gmail.com>
2025-05-16 15:44:50 +0530

    link hover

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,10 +3,8 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-center p-6">        
-            <h2 className="text-4xl font-semibold mb-2 text-red-600">Page Not Found</h2>
-            <p className="text-gray-700 mb-6">
-                Sorry, the page you are looking for doesn't exist or has been moved.
-            </p>
+            <h2 className="text-5xl font-semibold mb-2 text-red-600">Page Not Found</h2>
+           
             
         </div>
     );
