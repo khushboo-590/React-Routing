0de7c29ee3bb66e525de7ac9c19dad808283f79a
khushboo-590 <khushbook4979@gmail.com>
2025-05-17 08:57:12 +0530

    add apdding

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -7,7 +7,7 @@ import React from 'react';
 
 const ProductDetail = () => {
     const { id } = useParams();
-    const product = products.find((product) => product.id === parseInt(id));
+    
 
     if (!product) return <p className="text-3xl text-center">Product not found</p>;
 
