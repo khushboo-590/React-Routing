f51c15a7f0b2ffc02702b7b7d123e8ca33d041a3
khushboo-590 <khushbook4979@gmail.com>
2025-05-17 09:16:45 +0530

    string to number

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -7,7 +7,7 @@ import React from 'react';
 
 const ProductDetail = () => {
     const { id } = useParams();
-    
+    const product = products.find(product => product.id === Number(id));
 
     if (!product) return <p className="text-3xl text-center">Product not found</p>;
 
